13256d439bfad819506b3145b69c1fc10a1d1d66
georgii borisov <bugoriegor@yandex.com>
2024-09-23 14:46:20 +0200

    correct report cart

--- src/widgets/finance/report/UI/ReportCart.tsx
+++ src/widgets/finance/report/UI/ReportCart.tsx
@@ -1,4 +1,6 @@
 import ArrowTop from "@/assets/icons/arrow-top.svg?react";
+import { useState } from "react";
+import clsx from "clsx";
 
 interface report {
     time: Date,
@@ -15,7 +17,9 @@ interface props {
     data: Array<report>,
 }
 
-const BankCart = ({ date, receipt, writingOff, data }: props) => {
+const ReportCart = ({ date, receipt, writingOff, data }: props) => {
+
+    const [active, setActive] = useState<boolean>(true)
 
     const GetDate = (date: Date): string => {
         const day = date.getDate()
@@ -41,31 +45,38 @@ const BankCart = ({ date, receipt, writingOff, data }: props) => {
                 <span className="rounded-[13px] px-[15px] py-[8px] bg-[#FBFBFB] text-[12px] font-medium text-[#007BFB]">{GetDate(date)}</span>
             </div>
 
-            <div className="grow rounded-[26px] bg-[#ECEEF1] p-[15px]">
-                <div className="rounded-[13px] p-[13px] bg-[#FBFBFB] flex flex-col gap-[10px]">
-                    <div className="grid grid-cols-[1.5fr_4fr_2fr_2fr_22px] gap-[8px]  pb-[10px] border-0 border-b border-solid border-[#E5E7EA]">
-                        <span className="text-[#9B9FAD] text-[12px] font-medium">Время</span>
-                        <span className="text-[#9B9FAD] text-[12px] font-medium">Действие</span>
-                        <span className="text-[#9B9FAD] text-[12px] font-medium">Списание</span>
-                        <span className="text-[#9B9FAD] text-[12px] font-medium">Поступление</span>
-                        <ArrowTop className="w-[22px] h-[22px]" />
+            <div className="grow rounded-[26px] p-[15px] transition-all duration-500 bg-[#ECEEF1]">
+                <div className={clsx(" rounded-[13px] relative transition-all duration-500", !active && "bg-[#FBFBFB] p-[13px]")}>
+                    <ArrowTop onClick={() => setActive(!active)} className={clsx("cursor-pointer w-[22px] h-[22px] absolute top-[13px] right-[13px]  transition-all duration-500", active && "!top-[8px] rotate-[180deg]")} />
+                    <div className={clsx("max-h-[500px] overflow-hidden flex flex-col gap-[10px] pb-[10px] transition-all duration-500", active && "!max-h-[0px] !pb-[0px]")} >
+                        <div className="grid grid-cols-[1.5fr_4fr_2fr_2fr_22px] gap-[8px]  pb-[10px] border-0 border-b border-solid border-[#E5E7EA]">
+                            <span className="text-[#9B9FAD] text-[12px] font-medium">Время</span>
+                            <span className="text-[#9B9FAD] text-[12px] font-medium">Действие</span>
+                            <span className="text-[#9B9FAD] text-[12px] font-medium">Списание</span>
+                            <span className="text-[#9B9FAD] text-[12px] font-medium">Поступление</span>
+                            <span></span>
+                        </div>
+                        {
+                            data.map(item => (
+                                <div className="grid grid-cols-[1.5fr_4fr_2fr_2fr_22px] gap-[8px]  pb-[10px] border-0 border-b border-solid border-[#E5E7EA]">
+                                    <span className=" text-[12px] font-medium">{GetTime(item.time)}</span>
+                                    <p className=" text-[12px] font-normal">{item.act}</p>
+                                    <span className=" text-[12px] font-medium">{(item.receipt + '').replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')} ₽</span>
+                                    <span className=" text-[12px] font-medium">{(item.writingOff + '').replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')}  ₽</span>
+                                </div>
+                            ))
+                        }
                     </div>
-                    {
-                        data.map(item => (
-                            <div className="grid grid-cols-[1.5fr_4fr_2fr_2fr_22px] gap-[8px]  pb-[10px] border-0 border-b border-solid border-[#E5E7EA]">
-                                <span className=" text-[12px] font-medium">{GetTime(item.time)}</span>
-                                <p className=" text-[12px] font-normal">{item.act}</p>
-                                <span className=" text-[12px] font-medium">{(item.receipt + '').replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')} ₽</span>
-                                <span className=" text-[12px] font-medium">{(item.writingOff + '').replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')}  ₽</span>
-                            </div>
-                        ))
-                    }
 
                     <div className="grid grid-cols-[1.5fr_4fr_2fr_2fr_22px] gap-[8px]">
-                        <span className=" text-[12px] font-medium">Итого за день</span>
+                        <div><span className={clsx("text-[12px] font-medium transition-all duration-500", active && "opacity-0")}>Итого за день</span></div>
                         <div></div>
-                        <span className=" text-[12px] font-medium">{(receipt + '').replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')} ₽</span>
-                        <span className=" text-[12px] font-medium">{(writingOff + '').replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')}  ₽</span>
+                        <div><div className={clsx("inline-flex items-center gap-[15px] transition-all duration-500", active && "rounded-[13px] px-[15px] py-[8px] bg-[#FBFBFB]")}>
+                            <span className={clsx("text-[#9B9FAD] text-[12px] font-medium max-w-[0px] overflow-hidden transition-all duration-500", active && "!max-w-[100px]")}>Списание</span>
+                            <span className="text-[12px] font-medium">{(receipt + '').replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')} ₽</span></div></div>
+                        <div><div className={clsx("inline-flex items-center gap-[15px] transition-all duration-500", active && "rounded-[13px] px-[15px] py-[8px] bg-[#FBFBFB]")}>
+                            <span className={clsx("text-[#9B9FAD] text-[12px] font-medium max-w-[0px] overflow-hidden transition-all duration-500", active && "!max-w-[100px]")}>Поступление</span>
+                            <span className="text-[12px] font-medium">{(writingOff + '').replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')}  ₽</span></div></div>
                     </div>
                 </div>
             </div>
@@ -73,4 +84,4 @@ const BankCart = ({ date, receipt, writingOff, data }: props) => {
     );
 };
 
-export default BankCart;
+export default ReportCart;
